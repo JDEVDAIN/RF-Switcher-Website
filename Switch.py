85b0c8af9b23da7bd990e02a3aa153ad08e916b6
JDEVDAIN <JDEVDAIN@protonmail.com>
2022-05-13 12:58:39 +0200

    using fetch to update all switches

--- Switch.py
+++ Switch.py
@@ -1,5 +1,4 @@
 class Switch:
-
     enabled = False  # set enabled default to false
 
     def __init__(self, group: int, num: int, name: str, code_on: str, code_off: str, is_multi_switch=False):
@@ -15,3 +14,10 @@ class Switch:
 
     def __str__(self):
         return f'{self.num}: {self.name}: {self.enabled}'
+
+    def toSimpleJSON(self):
+        class simpleSwitch:  # inner class to make json.dumps work seamless
+            def __init__(self, num, enabled):
+                self.num = num
+                self.enabled = enabled
+        return simpleSwitch(self.num, self.enabled)
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+import json
 import os
-import Switch
 from flask import Flask, render_template, request
 import logging
 from Switches import SWITCH_LIST
@@ -67,42 +67,44 @@ def kill_switch():
                 rf_sender(switch.code_off)
 
 
-def switcher(request_dic):
-    for key, value in request_dic.items():
-        if value == "KILL":
-            kill_switch()
-            return
-        switch = find_switch(key)
-        if value == "ON":
-            if switch.is_multi_switch:
-                multi_switch_switcher(switch, value)
-            switch.enabled = True
-            app.logger.info(f'{switch.num}: {switch.name}: turned on')
-            #  print(f'{switch.num}: {switch.name}: turned on')
-            rf_sender(switch.code_on)
-        elif value == "OFF":
-            if switch.is_multi_switch:
-                multi_switch_switcher(switch, value)
-            switch.enabled = False
-            app.logger.info(f'{switch.num}: {switch.name}: turned off')
-            # print(f'{switch.num}: {switch.name}: turned off')
-            rf_sender(switch.code_off)
+def switcher(switchName, switchValue):
+    if switchValue == "KILL":
+        kill_switch()
+        return
+    switch = find_switch(switchName)
+    if switchValue == "ON":
+        if switch.is_multi_switch:
+            multi_switch_switcher(switch, switchValue)
+        switch.enabled = True
+        app.logger.info(f'{switch.num}: {switch.name}: turned on')
+        #  print(f'{switch.num}: {switch.name}: turned on')
+        rf_sender(switch.code_on)
+    elif switchValue == "OFF":
+        if switch.is_multi_switch:
+            multi_switch_switcher(switch, switchValue)
+        switch.enabled = False
+        app.logger.info(f'{switch.num}: {switch.name}: turned off')
+        # print(f'{switch.num}: {switch.name}: turned off')
+        rf_sender(switch.code_off)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/api/switches", methods=['POST'])
+def switcherApi():
+    if request.method == 'POST':  # maybe make two endpoints
+        if request.is_json:
+            changed_switch = request.get_json()
+            print(changed_switch)
+            switcher(changed_switch["name"], changed_switch["value"])
+            flat_switch_list = [switch for group_list in SWITCH_LIST for switch in group_list]
+            json_switch_list = [switch.toSimpleJSON() for switch in flat_switch_list]
+            print(({'switch_list': json_switch_list}))
+            return json.dumps({"switch_list": json_switch_list}, default=vars), 200, {'ContentType': 'application/json'}
+
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        request_dic = request.form.to_dict()
-        app.logger.info("Request: " + str(request.form.to_dict()))
-        if len(request_dic) <= 0:
-            pass
-        else:
-            switcher(request_dic)
-
-    elif request.method == 'GET':
-        return render_template("index.html", switches=Switch_list, form=request.form)
-
-    return render_template("index.html", switches=Switch_list)
+    if request.method == 'GET':
+        return render_template("index.html", switches=SWITCH_LIST)
 
 
 if __name__ == '__main__':
